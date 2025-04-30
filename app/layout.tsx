ae2b9bdd9e5d6da19012874dfe9bb5f43bd57a54
Charles Montoya <charles.j.montoya@gmail.com>
2025-04-30 16:23:22 -0400

    added open graph and favicon data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Head from "next/head";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -13,9 +14,29 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
+  icons: {
+    icon: "/localworx-logo.jpg",
+  },
   title: "LocalWorx | Empowering Communities Through Peer-Powered Promotion",
   description:
     "LocalWorx.io is a decentralized platform that helps local businesses grow through word-of-mouth marketing, Bitcoin micro-payments, and community collaboration. Promote, earn, and support local resilience — without the gatekeepers.",
+  openGraph: {
+    title: "LocalWorx | Empowering Communities Through Peer-Powered Promotion",
+    description:
+      "LocalWorx.io is a decentralized platform that helps local businesses grow through word-of-mouth marketing, Bitcoin micro-payments, and community collaboration. Promote, earn, and support local resilience — without the gatekeepers.",
+    url: "https://LocalWorx.io",
+    siteName: "LocalWorx",
+    images: [
+      {
+        url: "https://localworx.io/localworx-logo-192x192.png",
+        width: 192,
+        height: 192,
+        alt: "LocalWorx Logo",
+        type: "image/png",
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function RootLayout({
@@ -28,6 +49,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Head>
+          <meta
+            property="og:image"
+            content="https://localworx.io/localworx-logo.jpg"
+          />
+        </Head>
         {children}
       </body>
     </html>
