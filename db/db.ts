650a867a6fadc303ecbaafcd967f8b79f93e2602
Charles Montoya <charles.j.montoya@gmail.com>
2025-05-02 20:42:44 -0400

    added environment awareness to db file.

--- db/db.ts
+++ db/db.ts
@@ -2,10 +2,14 @@ import knex from "knex";
 
 import knexfile from "./knexfile";
 
+type KnexConfigKeys = "development" | "staging" | "production";
+
 // TODO: In Prod, don't access knexfile.development directly
 // but use environment variables for database connection
 // and set the environment variable NODE_ENV=production
-// to use the production database
-const db = knex(knexfile.development);
+const environment = process.env.NODE_ENV || "development";
+const config = knexfile[environment as KnexConfigKeys];
+
+const db = knex(config);
 
 export default db;
