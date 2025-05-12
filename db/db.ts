a2106a4e2b2cd5e5d6a90fb0ccce073a7be97977
Charles Montoya <charles.j.montoya@gmail.com>
2025-05-12 15:12:31 -0400

    fixed db.ts file

--- db/db.ts
+++ db/db.ts
@@ -1,15 +1,18 @@
-import knex from "knex";
-
+import knex, { Knex } from "knex";
 import knexfile from "./knexfile";
 
-// type KnexConfigKeys = "development" | "staging" | "production";
-
-// TODO: In Prod, don't access knexfile.development directly
-// but use environment variables for database connection
-// and set the environment variable NODE_ENV=production
 const environment = process.env.NODE_ENV || "development";
 const config = knexfile[environment as keyof typeof knexfile];
 
-const db = knex(config);
+// Prevent multiple instances in development (hot reload)
+declare global {
+  var __knex: Knex | undefined;
+}
+
+const db = global.__knex ?? knex(config);
+
+if (process.env.NODE_ENV !== "production") {
+  global.__knex = db;
+}
 
 export default db;
