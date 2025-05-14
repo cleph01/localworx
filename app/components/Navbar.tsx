ba0df9843742bcdf5f152e7440e3fe487a59c516
Charles Montoya <charles.j.montoya@gmail.com>
2025-05-13 23:44:23 -0400

    updated homepage

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
-    <nav className="bg-navy-background text-white shadow-lg sticky top-0 z-50">
+    <nav className="bg-navy-blue-background text-white shadow-lg sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0 p-1 bg-white rounded">
--- app/page.tsx
+++ app/page.tsx
@@ -490,8 +490,38 @@ export default function Home() {
         </p>
       </section>
       {/* Footer */}
-      <section className="min-w-screen bg-navy-blue-background">
-        <Image src="/localworx-text-only.svg" alt="" width={100} height={50} />
+      <section className="p-4 min-w-screen bg-navy-blue-background text-gray-500 text-lg">
+        <Image
+          src="/localworx-text-only.svg"
+          className="my-6"
+          alt=""
+          width={150}
+          height={75}
+        />
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est
+          excepturi pariatur cum atque quis quibusdam, nesciunt earum voluptate
+          harum esse ea vel sequi at molestias iusto magni error quidem
+          delectus.
+        </p>
+        <div className="space-y-2 mb-12">
+          <h5 className="text-white font-bold text-xl mt-10">Navigation</h5>
+          <p>Home</p>
+          <p>About</p>
+          <p>Services Directory</p>
+          <p>Promoter Hub</p>
+          <p>Marketplace</p>
+          <p>Help</p>
+
+          <h5 className="text-white font-bold text-xl mt-10">Contact</h5>
+          <p>support@localworx.io</p>
+          <p>(212)555-5555</p>
+
+          <h5 className="text-white font-bold mt-10">Newsletter</h5>
+          {/* Add Newsletter component */}
+          <p>Stay up to date with promos and announcements</p>
+        </div>
+        <p>&copy; 2025 LocalWorx. All Rights Reserved</p>
       </section>
       {/* GitHub CTA */}
       <a
