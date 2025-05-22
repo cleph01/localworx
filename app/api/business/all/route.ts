7a4d9792703442da04b8f73773b0a329491de1fc
Charles Montoya <charles.j.montoya@gmail.com>
2025-05-22 03:41:24 -0400

    fixed import path location for businesses controller

--- app/api/business/all/route.ts
+++ app/api/business/all/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest } from "next/server";
-import { getBusinessesHandler } from "../businessesController";
+import { getBusinessesHandler } from "./businessesController";
 
 export async function GET(req: NextRequest) {
   return getBusinessesHandler(req);
