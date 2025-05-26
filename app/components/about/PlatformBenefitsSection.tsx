4297a8de37114730a0b5115123edcdb69fcb72da
Charles Montoya <charles.j.montoya@gmail.com>
2025-05-26 19:25:02 -0400

    small change to text-left some verbiage

--- app/components/about/PlatformBenefitsSection.tsx
+++ app/components/about/PlatformBenefitsSection.tsx
@@ -5,14 +5,14 @@ const PlatformBenefitsSection = () => {
       <h2 className="text-4xl font-bold text-center mb-6">
         More Than Just Another Gig App
       </h2>
-      <p className="text-gray-600 text-lg max-w-3xl mb-4">
+      <p className="text-gray-600 text-lg max-w-3xl mb-4 ">
         Whether you're a promoter or a business, your visibility goes far beyond
         LocalWorx. Continue creating content on YouTube, X, and the other
         platforms. Make as much Adsense money as you can - but now include a
         link to LocalWorx so that viewers can{" "}
         <span className="font-semibold">Thank-Zap</span> you too.
       </p>
-      <p className="text-gray-600 text-lg max-w-3xl text-center">
+      <p className="text-gray-600 text-lg max-w-3xl">
         You keep your AdSense and your channel growth while earning Bitcoin from
         your audience too.
       </p>
