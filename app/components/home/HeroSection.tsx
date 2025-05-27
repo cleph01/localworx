534e465605b53e5372e546c9701bf219dbd5e239
Charles Montoya <charles.j.montoya@gmail.com>
2025-05-27 03:46:06 -0400

    Added the intro video to the home page

--- app/components/home/HeroSection.tsx
+++ app/components/home/HeroSection.tsx
@@ -10,18 +10,25 @@ const HeroSection = () => {
       {/* Hero Image */}
       <LazyLoadWrapper
         fallback={
-          <div className="w-full h-48 w-48 mt-2 rounded-xl border border-gray-200 bg-gray-200 animate-pulse" />
+          <div className="w-full h-64 w-64 mt-2 rounded-xl border border-gray-200 bg-gray-200 animate-pulse" />
         }
         delayMs={200}
         timeoutMs={5000}
+        className="w-full flex flex-col items-center h-96 w-78 mt-2 rounded-xl mb-2"
       >
-        <Image
+        <iframe
+          className="w-full h-88 max-w-3xl mt-2 rounded-xl border border-gray-200 object-cover shadow-sm"
+          src="https://www.youtube.com/embed/P2phABZ8_88"
+          title="YouTube Preview"
+          allowFullScreen
+        />
+        {/* <Image
           src="/localworx-logo.png"
           alt="LocalWorx Logo"
           className="w-48 h-auto my-6 "
           width={192}
           height={192}
-        />
+        /> */}
       </LazyLoadWrapper>
 
       <p className="max-w-4xl sm:text-2xl text-gray-500 mb-4">
