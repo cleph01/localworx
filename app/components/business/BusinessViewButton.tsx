600b71e261a6d00475fce7628c9efa1d9b688a7e
Charles Montoya <charles.j.montoya@gmail.com>
2025-06-03 11:38:28 -0400

    made the "view business profile" button w-full

--- app/components/business/BusinessViewButton.tsx
+++ app/components/business/BusinessViewButton.tsx
@@ -13,9 +13,9 @@ const BusinessViewButton = ({ businessId }: { businessId: string }) => {
   return (
     <button
       onClick={handleViewItem}
-      className="bg-gray-200 text-gray-800 px-3 py-1 rounded text-sm flex items-center gap-1 hover:bg-gray-300 cursor-pointer"
+      className="w-full bg-gray-200 text-gray-800 px-3 py-1 rounded text-sm flex items-center justify-center gap-1 hover:bg-gray-300 cursor-pointer"
     >
-      <FaEye /> View
+      <FaEye /> View Business Profile
     </button>
   );
 };
